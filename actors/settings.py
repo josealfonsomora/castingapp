55b85e3f375fb0c98165e83c871c5d600c1628dd
Oscar Carballal Prego <oscar.carballal@protonmail.com>
2020-02-11 12:39:11 +0100

    Correeect settings and disable preious keys and pss

--- actors/settings.py
+++ actors/settings.py
@@ -84,11 +84,11 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'NAME': 'casting',
+        'NAME': '',
         'ENGINE': 'django.db.backends.postgresql',
         'USER': 'postgres',
-        'PASSWORD': 'root',
-        'HOST': '172.17.0.4',
+        'PASSWORD': '',
+        'HOST': '',
         'PORT': '5432',
     }
 }
@@ -122,8 +122,8 @@ USE_L10N = True
 USE_TZ = True
 
 # django-storages
-AWS_ACCESS_KEY_ID = 'AKIA5VJ4ODGHHSID6OSN'
-AWS_SECRET_ACCESS_KEY = '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'
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
 AWS_STORAGE_BUCKET_NAME = 'safehouse-actors'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_FILE_OVERWRITE = False
